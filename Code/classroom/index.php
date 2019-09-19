5b7f6c7d05efb02efef9ed06beefb4c23b22b202
rospino74 <rospino74gmail.com>
2019-09-19 15:45:21 +0200

    Update classroop page

--- Code/classroom/index.php
+++ Code/classroom/index.php
@@ -4,8 +4,8 @@
 	<meta name="charset" value="utf-8">
 	<meta name="theme-color" content="#53e300">
 	<meta name="Author" content="Marko">
-	<meta name="Description" content="Last post for <? echo $_GET['class']; ?>" />
-	<title><? echo $_GET['class']; ?> | Student</title>
+	<meta name="Description" content="Last posts for <?php echo $_GET['class']; ?>" />
+	<title><?php echo $_GET['class']; ?> | Student</title>
 	<link rel="shortcut icon" href="../rsc/favicon.ico" type="image/x-icon">
 	<link href="../rsc/icon-hires.png" rel="icon" sizes="192x192" />
 	<link href="../rsc/icon.png" rel="icon" sizes="128x128" />
@@ -25,43 +25,38 @@
 	</style>
 </head>
 <body>
-<?
-include '../db.config.php';
-$classe = $_GET['class'];
-if($menu == '') {
-echo <<<EOD
 <div class="navbar">
   <a href="../home.php" class="menu-item" id="home"><img style='display:none;' src="../rsc/icon_hires.png" />Student</a>
   <a href="#last">Ultimo Post</a>
   <a href="../check.php" style="float:right;">Esci</a>
 </div>
-EOD;
-};
-$action = $_GET['a'];
+<?php
+include '../db.config.php';
+$classe = $_GET['class'];
+$action = isset($_GET['action']) ? $_GET['action'] : null;
 switch($action) {
 //caso niente
 default:
-$sql = 'SELECT * FROM ' . $tabellanews;
+$sql = 'SELECT * FROM ' . $classe;
 $query = mysqli_query($connessione, $sql);
 echo '<div align="center" style="margin-top: 10%;"><h1 style="color: #33cc33; font-family: Architects Daughter;">Ultimi post per '.$classe.'</h1></div>';
  while($data = mysqli_fetch_array($query)) {
  $id = $data['id'];
- $time = $data['date'];
+ $date = $data['date'];
  $title = $data['title'];
- $contnent = $data['contnent'];
+ $content = $data['content'];
  $author = $data['author'];
 //ora inserisco la tabella
-
  echo <<<EOD
  <div class="content">
 	<div class="title">
 		<h3>$title</h3>
 	</div>
     <div class="text">
-    	<p>$contnent</p>
+    	<p>$content</p>
     </div>
 	<div class="info">
-		$author	$data
+		$author	- $date
 	</div>	
  </div>
 EOD;
@@ -85,7 +80,7 @@ EOD;
 } else {
  $title = $_POST['titolo'];	
  $text = $_POST['testo'];
- $sql = "INSERT INTO `".$dbname."`.`" . $classe . "` (`id`, `date`, `title`,`contnent`, `ip_host`, `author`) VALUES (NULL, '".$data."', '".$title."', '".$text."', '".$_SERVER['REMOTE_ADDR']."', '".$username."')";
+ $sql = "INSERT INTO `".$dbname."`.`" . $classe . "` (`id`, `date`, `title`,`content`, `ip_host`, `author`) VALUES (NULL, '".$data."', '".$title."', '".$text."', '".$_SERVER['REMOTE_ADDR']."', '".$username."')";
  $query = mysqli_query($connessione, $sql);
  header('HTTP/1.0 200 Ok');
 };
