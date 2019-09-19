c0d7465f6f53dafcd72c97f8e1ac1a02ce7eca7a
rospino74 <rospino74gmail.com>
2019-09-19 14:24:40 +0200

    Removed warnings

--- Code/check.php
+++ Code/check.php
@@ -1,3 +1,29 @@
+<?php
+$err = false;
+
+	if(isset($_POST['user'])) {
+		$user = $_POST['user'];
+  		$pw = $_POST['pw'];
+  		$sql1 = "SELECT * FROM `acesso` WHERE `nome` = ".$user.";";
+  		$userame = mysqli_fetch_array($sql1);
+  		$query = mysqli_query($connessione, "SELECT id FROM acesso WHERE nome='$user' and password='" . md5($pw) ."'");
+  		/*$query = mysqli_query($sql);*/
+  		$num = $query->num_rows;
+  		if($num == 1) {
+  			setcookie("login", 1, time() + 86400);
+  			setcookie("username", $username['persona'], time() + 86395);
+  			header('HTTP/1.1 200 OK');
+ 			header('Location: index.php?user='.$user);
+			
+			exit;
+			
+  			//se sbagliato
+  		} else {
+  			header('HTTP/1.1 403 Forbidden');
+			$err = true;
+		}
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +36,7 @@
     <meta name="MobileOptimized" content="176" />
   	<meta name="viewport" content="width=device-width, user-scalable=0, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
     <style>
-    	#logout {
+    	.logout {
 			padding: 2%;
     		color: black;
     		background-color: #bdcddc;
@@ -21,22 +47,34 @@
             height: 50px;
             font-family: 'courier new', courier, monospace;
 		}
+		.err-login {
+			padding: 2%;
+    		color: black;
+    		background-color: #ff5555;
+    		border-left: 5px #f90 solid;
+    		text-align:center;
+            margin: 2% auto;
+			width: 180px;
+            height: 50px;
+            font-family: 'courier new', courier, monospace;
+		}
     </style>
 </head>
 <body>
 <?php
-$job = $_GET['logout'];
-	setcookie(login, 1, time() - 36000);
-	setcookie(username, Gg, time() - 36000);
- 	if(!isset($_POST['user'])) {
- ?>
+	$job = $_GET['logout'];
+	setcookie("login", 0, time() - 36000);
+	setcookie("username", NULL, time() - 36000);
+?>
 <div class="centrato">
 	<p style="text-align: center; font-size: 18pt; font-family: terminal, monaco, monospace; color: #3C3; font-weight: bold;">Accesso</p>
 <?	if(isset($job)) {
-		setcookie(login, 1, time() - 36000);
-		setcookie(username, Gg, time() - 36000);
-        echo '<div id="logout">logout eseguito con successo!</div>';
-};
+		setcookie("login", 0, time() - 36000);
+		setcookie("username", NULL, time() - 36000);
+        echo '<div class="logout">Logout eseguito con successo!</div>';
+	} else if($err) {
+		echo '<div class="err-login">Credenziali Errate!</div>';
+	}
 ?>
     <form action="" method="POST" class="centrato">
 		<p style="text-align: center;">
@@ -49,26 +87,5 @@ $job = $_GET['logout'];
     	</p>
 	</form>
 </div>
-<?
-	} else {
-  		$user = $_POST['user'];
-  		$pw = $_POST['pw'];
-  		$sql1 = "SELECT * FROM `acesso` WHERE `nome` = ".$user.";";
-  		$userame = mysqli_fetch_array($sql1);
-  		$query = mysqli_query($connessione, "SELECT id FROM acesso WHERE nome='$user' and password='" . md5($pw) ."'");
-  		/*$query = mysqli_query($sql);*/
-  		$num = $query->num_rows;
-  		if($num == 1) {
-  			setcookie(login, 1, time() + 86400);
-  			setcookie(username, $username['persona'], time() + 86395);
-  			header('HTTP/1.1 200 OK');
- 			header('Location: index.php?user='.$user);
-  			//se sbagliato
-  		} else {
-  			header('HTTP/1.1 403 Forbidden');
-  			echo '<h4 style="color: red; text-align: center; font-family: Pricedown; font-size: 128dp;">Coppia<br /><i>Utente / Password errata</i><br /> o non specificata!</h4>';
-		};
- }
-?>
 </body>
 </html>
