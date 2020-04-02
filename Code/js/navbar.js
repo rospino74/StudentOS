d22f7404543565217c2a7871e7c160115d5606b3
rospino74 <rospino74gmail.com>
2020-04-02 10:10:05 +0200

    New scrolltop button in navbar

--- Code/js/navbar.js
+++ Code/js/navbar.js
@@ -1,6 +1,7 @@
 const back = document.querySelectorAll('.navbar *[data-action=back]');
 const quit = document.querySelectorAll('.navbar *[data-action=quit]');
 const home = document.querySelectorAll('.navbar *[data-action=home]');
+const scrollTop = document.querySelectorAll('.navbar *[data-action=scrollTop]');
 
 back.forEach((item) => {
 	item.addEventListener("click", (e) => {
@@ -17,5 +18,11 @@ quit.forEach((item) => {
 home.forEach((item) => {
 	item.addEventListener("click", (e) => {
 		window.location.href = "../";
+	});
+});
+
+scrollTop.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		window.scrollTo(0,0);
 	});
 });
